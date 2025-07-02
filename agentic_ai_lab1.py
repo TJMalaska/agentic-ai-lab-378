008e0502fd883c3237c6b5d41141e7995e63e39e
TJMalaska <malaskatj@gmail.com>
2025-07-01 17:44:10 -0700

    finished lab 1

--- agentic_ai_lab1.py
+++ agentic_ai_lab1.py
@@ -2,11 +2,12 @@ import ollama
 
 try:
     response = ollama.chat(
-        model='misteral',
+        model="mistral",
         messages=[
-            {role: 'user', content: 'Explain the CIA Triad in cybersecurity.'}]
+            {"role": "user", "content": "Explain the CIA Triad in cybersecurity."}
+        ],
     )
-    print(response['message']['content'])
+    print(response["message"]["content"])
 except Exception as e:
     print(f"An error occured: {e}")
     print("Please ensure the Ollama application is running.")
